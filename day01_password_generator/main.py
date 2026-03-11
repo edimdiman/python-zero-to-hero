a8a54b25718a127bec7272e875497653a1846cae
Edward Dimdiman <edimdim@wgu.edu>
2026-03-11 14:45:20 -0700

    moved first day project to correct folder

---
+++ day01_password_generator/main.py
@@ -0,0 +1,12 @@
+# And so it begins..
+
+# Uh, what do I need?
+# secure way to generator truly random characters
+# ask how long the password should be?
+# maybe complex -> ask what special characters that need to be used 
+# ask what special characters you can't use? 
+# store [terrible advice in real life, but you know, it's a intro project fam] 
+#   passwords, e.g. remember the last 10 passwords. 
+
+# maybe for later -> password protect this generator 
+# still not secure for sure 
