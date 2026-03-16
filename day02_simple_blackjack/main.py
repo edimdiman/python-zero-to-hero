972bf1b0b04eba081b029bfba8613355ebb9cd76
Edward Dimdiman <edimdim@wgu.edu>
2026-03-15 19:37:35 -0700

    progress on blackjack game

--- day02_simple_blackjack/main.py
+++ day02_simple_blackjack/main.py
@@ -1,1 +1,25 @@
 # a super simple blackjack game will live here
+
+import secrets 
+
+player_score = 0
+pc_score = 0
+
+while True: 
+    pc_upcard = secrets.choice(range(1,12))
+    pc_downcard = secrets.choice(range(1,12))
+    player_upcard = secrets.choice(range(1,12))
+
+    if pc_upcard >= 10: 
+        # add insurance later
+        if pc_downcard == 11:
+            # add cool spinning or dot animation here eventually, seems too 
+            # complicated at the moment
+            print("Dealer has a blackjack, better luck next time")
+            break
+        else:
+            print("Nobody is home, game goes on")
+
+
+            
+
